956b0029d04a79f6007cda488faf53fa07c97863
PiotrT4 <tarnavsky.piotr@gmail.com>
2025-08-15 17:47:21 +0200

    Added new scnario for POST request

--- src/test/java/stepDefinitions/PostSteps.java
+++ src/test/java/stepDefinitions/PostSteps.java
@@ -1,4 +1,44 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import utils.ApiUtils;
+import utils.BookingBodyGenerator;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class PostSteps {
+    public static String requestBody;
+
+    @Given("Generate booking data")
+    public void generate_booking_data() {
+        requestBody = BookingBodyGenerator.generateBookingBody();
+    }
+
+    @When("The user send POST request with generated data")
+    public void the_user_send_get_request_with_generated_data() {
+        ApiUtils.response = RestAssured
+                .given()
+                    .contentType("application/json")
+                    .body(requestBody)
+                .post("/booking");
+    }
+
+    @And("The response should have created booking details")
+    public void the_response_should_have_created_booking_details() {
+        assertAll(
+                "Response validation:",
+                () -> assertNotNull(ApiUtils.response.jsonPath().getString("bookingid")),
+                () -> assertNotNull(ApiUtils.response.jsonPath().getString("booking.firstname")),
+                () -> assertNotNull(ApiUtils.response.jsonPath().getString("booking.lastname")),
+                () -> assertNotNull(ApiUtils.response.jsonPath().getString("booking.totalprice")),
+                () -> assertNotNull(ApiUtils.response.jsonPath().getString("booking.depositpaid")),
+                () -> assertNotNull(ApiUtils.response.jsonPath().getString("booking.bookingdates.checkin")),
+                () -> assertNotNull(ApiUtils.response.jsonPath().getString("booking.bookingdates.checkout")),
+                () -> assertNotNull(ApiUtils.response.jsonPath().getString("booking.additionalneeds"))
+        );
+    }
 }
---
+++ src/test/resources/features/CreateBooking.feature
@@ -0,0 +1,6 @@
+Feature: Create a new bookings with basic information
+
+  Scenario: User positive creates a new booking
+    Given Generate booking data
+    When The user send POST request with generated data
+    Then The response status code should be 200
